bcae71dd68ddef3197bc6ed632fe7e495524c9b7
kpzr7 <clouet.anthony@outlook.fr>
2017-03-03 11:27:47 +0100

    bonsoir

--- TP2_ex5/script.js
+++ TP2_ex5/script.js
@@ -1,14 +1,14 @@
-(function($){
+
+$( document ).ready(function($){
 
     /* Quand je clique sur l'icône hamburger je rajoute une classe au body */
-    $('#header__icon').click(function(e){
-        e.preventDefault();
-        $('body').toggleClass('with--sidebar');
+    $('#menu-icon').click(function(e){
+        $('#burger').toggle();
     });
 
     /* Je veux pouvoir masquer le menu si on clique sur le cache */
     $('#site-cache').click(function(e){
-        $('body').removeClass('with--sidebar');
+    $('body').removeClass('with--sidebar');
     })
+});
 
-})(jQuery);
